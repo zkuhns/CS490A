54c0c4aaa34840fe90598392f2b01b486831e502
zkuhns <kuhns.zachery@gmail.com>
2016-05-18 16:05:14 -0600

    Fixed indentation on lab3.

--- Lab3/main.s
+++ Lab3/main.s
@@ -30,78 +30,71 @@ GPIO_PORTE_AMSEL_R      EQU   0x40024528
 GPIO_PORTE_PCTL_R       EQU   0x4002452C
 SYSCTL_RCGCGPIO_R       EQU   0x400FE608
 
-       IMPORT  TExaS_Init
-       AREA    |.text|, CODE, READONLY, ALIGN=2
-       THUMB
-       EXPORT  Start
-Start
- ; TExaS_Init sets bus clock at 80 MHz
-      BL  TExaS_Init ; voltmeter, scope on PD3
-; you initialize PE1 PE0
-      CPSIE  I    ; TExaS voltmeter, scope runs on interrupts
+    IMPORT  TExaS_Init
+    AREA    |.text|, CODE, READONLY, ALIGN=2
+    THUMB
+    EXPORT  Start
+Start         ; TExaS_Init sets bus clock at 80 MHz
+              BL  TExaS_Init ; voltmeter, scope on PD3
+              ; you initialize PE1 PE0
+              CPSIE  I    ; TExaS voltmeter, scope runs on interrupts
 	  
-	  BL    PortE_Init
-	  B     loop
+              BL    PortE_Init
+              B     loop
 	  
-PortE_Init
-	LDR    R0,    =SYSCTL_RCGCGPIO_R
-	MOV    R1,    #0x10
-	STR    R1,    [R0]
+PortE_Init    LDR    R0,    =SYSCTL_RCGCGPIO_R
+              MOV    R1,    #0x10
+              STR    R1,    [R0]
 
-    LDR    R0,    =GPIO_PORTE_DATA_R
-	MOV    R1,    #0x01
-	STR    R1,    [R0]
+              LDR    R0,    =GPIO_PORTE_DATA_R
+              MOV    R1,    #0x01
+              STR    R1,    [R0]
 	
-	LDR    R0,    =GPIO_PORTE_DIR_R
-	MOV    R1,    #0x01
-	STR    R1,    [R0]
+              LDR    R0,    =GPIO_PORTE_DIR_R
+              MOV    R1,    #0x01
+              STR    R1,    [R0]
 	
-	LDR    R0,    =GPIO_PORTE_AFSEL_R
-	MOV    R1,    #0x00
-	STR    R1,    [R0]
+              LDR    R0,    =GPIO_PORTE_AFSEL_R
+              MOV    R1,    #0x00
+              STR    R1,    [R0]
 	
-	LDR    R0,    =GPIO_PORTE_DEN_R
-	MOV    R1,    #0x03
-	STR    R1,    [R0]
+              LDR    R0,    =GPIO_PORTE_DEN_R
+              MOV    R1,    #0x03
+              STR    R1,    [R0]
 	
-	LDR    R0,    =GPIO_PORTE_AMSEL_R
-	MOV    R1,    #0x00
-	STR    R1,    [R0]
+              LDR    R0,    =GPIO_PORTE_AMSEL_R
+              MOV    R1,    #0x00
+              STR    R1,    [R0]
 	
-	LDR    R0,    =GPIO_PORTE_PCTL_R
-	MOV    R1,    #0x00
-	STR    R1,    [R0]
+              LDR    R0,    =GPIO_PORTE_PCTL_R
+              MOV    R1,    #0x00
+              STR    R1,    [R0]
 	
-    BX    LR
+              BX    LR
 	
 ; R0 address to data
 ; R1 data register
 ; R2 state on/off
 ; R3 delay decrement value
-loop
-	LDR    R0,    =GPIO_PORTE_DATA_R      ; Read address of data into R0
-	LDR    R1,    [R0]                    ; Copy data contents into R1
-	AND    R2,    R1,    #0x02            ; Check if the switch is on
-	CMP    R2,    #0x02                   ; Comparison to see if it is on
-	BEQ    delay                          ; If it is on go to the delay tag
-	BNE    turnoff                        ; If it is off go to the turnoff tag
-delay
-	LDR    R3,    =0x0014C080             ; Load 272000 into R3
-wait
-	SUBS   R3,    R3,    #1               ; Subtract one from R3 until 0
-	BNE    wait                           ; Repeat decrement
-switch
-	AND    R2,    R1,    #0x01            ; Check if the light is on
-	CMP    R2,    #0x01                   ; Comparison if the light is on
-	SUBEQ  R1,    #1                      ; If it is on turn it off
-	ADDNE  R1,    #1                      ; If it is off turn it on
-	STR    R1,    [R0]                    ; Store contents of R1 back into data register
-	B      loop                           ; Go back to loop tag
-turnoff
-	MOV    R1,    #0x01                   ; Turn the led on
-	STR    R1,    [R0]                    ; Store contents of R1 into data register
-	B      loop                           ; Go back to loop tag
+loop          LDR    R0,    =GPIO_PORTE_DATA_R      ; Read address of data into R0
+              LDR    R1,    [R0]                    ; Copy data contents into R1
+              AND    R2,    R1,    #0x02            ; Check if the switch is on
+              CMP    R2,    #0x02                   ; Comparison to see if it is on
+              BEQ    delay                          ; If it is on go to the delay tag
+              BNE    turnoff                        ; If it is off go to the turnoff tag
+delay         LDR    R3,    =0x0014C080             ; Load 272000 into R3
+wait          SUBS   R3,    R3,    #1               ; Subtract one from R3 until 0
+              BNE    wait                           ; Repeat decrement
+switch        AND    R2,    R1,    #0x01            ; Check if the light is on
+              CMP    R2,    #0x01                   ; Comparison if the light is on
+              SUBEQ  R1,    #1                      ; If it is on turn it off
+              ADDNE  R1,    #1                      ; If it is off turn it on
+              STR    R1,    [R0]                    ; Store contents of R1 back into data register
+              B      loop                           ; Go back to loop tag
+turnoff       MOV    R1,    #0x01                   ; Turn the led on
+              STR    R1,    [R0]                    ; Store contents of R1 into data register
+              B      loop                           ; Go back to loop tag
 
-      ALIGN      ; make sure the end of this section is aligned
-      END        ; end of file
+    ALIGN      ; make sure the end of this section is aligned
+    END        ; end of file
        
